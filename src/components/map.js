7b2fc61b8af03596cc41fd408c3851c483d07d4b
Rodrigo Moreira <hola@piducan.dev>
2021-02-09 20:41:01 -0300

    react-map-gl/issues/1266#issuecomment-753686953

--- src/components/map.js
+++ src/components/map.js
@@ -4,13 +4,13 @@ import { easeCubic } from "d3-ease"
 import { Scrollama, Step } from "react-scrollama"
 import mapboxgl from "mapbox-gl" // This is a dependency of react-map-gl even if you didn't explicitly install it
 
-// eslint-disable-next-line import/no-webpack-loader-syntax
-mapboxgl.workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default
-
 import useStore from "./viewport"
 import { results } from "./cerrosMaule.json"
 
 import "mapbox-gl/dist/mapbox-gl.css"
+
+// eslint-disable-next-line import/no-webpack-loader-syntax
+mapboxgl.workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default
 
 const chapters = results.map(cerro => ({
   ...cerro,
