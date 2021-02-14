aaabba15f21938172529be018364e5e32c214f52
Rodrigo Moreira <hola@piducan.dev>
2021-02-14 10:52:02 -0300

    add rotation onTransitionEnd

--- src/components/map.js
+++ src/components/map.js
@@ -1,5 +1,6 @@
 import React, { useRef } from "react"
-import ReactMapGL from "react-map-gl"
+import ReactMapGL, { Layer, LinearInterpolator } from "react-map-gl"
+import { easeLinear } from "d3-ease"
 import mapboxgl from "mapbox-gl" // This is a dependency of react-map-gl even if you didn't explicitly install it
 
 import useStore from "./viewport"
@@ -9,43 +10,57 @@ import "mapbox-gl/dist/mapbox-gl.css"
 // eslint-disable-next-line import/no-webpack-loader-syntax
 mapboxgl.workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default
 
+const skyLayer = {
+  id: "sky",
+  type: "sky",
+  paint: {
+    "sky-opacity": ["interpolate", ["linear"], ["zoom"], 0, 0, 5, 0.3, 8, 1],
+    "sky-type": "atmosphere",
+    "sky-atmosphere-sun": [0.0, 0.0],
+    "sky-atmosphere-sun-intensity": 3,
+  },
+}
+
 export default function Map(props) {
   const { viewport, setViewport } = useStore()
   const ref = useRef()
-  const handleLoaded = () => {
+  const onLoad = () => {
     const map = ref.current.getMap()
-
     map.addSource("mapbox-dem", {
       type: "raster-dem",
       url: "mapbox://mapbox.mapbox-terrain-dem-v1",
       tileSize: 512,
       maxzoom: 14,
     })
-    // add the DEM source as a terrain layer with exaggerated height
     map.setTerrain({ source: "mapbox-dem", exaggeration: 1.6 })
-    // add a sky layer that will show when the map is highly pitched
-    map.addLayer({
-      id: "sky",
-      type: "sky",
-      paint: {
-        "sky-type": "atmosphere",
-        "sky-atmosphere-sun": [0.0, 0.0],
-        "sky-atmosphere-sun-intensity": 15,
-      },
-    })
   }
-
+  const onTransitionEnd = () =>
+    setViewport({
+      bearing: 720,
+      transitionDuration: 90000,
+      transitionInterpolator: new LinearInterpolator(),
+      transitionEasing: easeLinear,
+    })
   return (
     <ReactMapGL
       {...viewport}
       {...props}
       ref={ref}
-      height="100%"
-      width="100%"
-      onLoad={handleLoaded}
+      height="100vh"
+      width="100vw"
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        zIndex: -500,
+      }}
       onViewportChange={setViewport}
+      onLoad={onLoad}
+      onTransitionEnd={onTransitionEnd}
       mapStyle="mapbox://styles/mapbox/satellite-v9"
       mapboxApiAccessToken={process.env.GATSBY_MAPBOX_ACCESS_TOKEN}
-    />
+    >
+      <Layer {...skyLayer} />
+    </ReactMapGL>
   )
 }
