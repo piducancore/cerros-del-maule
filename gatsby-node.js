7f5ec925aa62d546200ed4522bd6f99a14a3d6be
Rodrigo Moreira <hola@piducan.dev>
2021-02-26 15:01:30 -0300

    create root story from files in stories folder

--- gatsby-node.js
+++ gatsby-node.js
@@ -30,18 +30,21 @@ exports.createPages = async ({ graphql, actions }) => {
   `)
 
   const stories = _.groupBy(
-    data.allMdx.nodes.map(story => ({
-      ...story,
-      relativePath: story.slug.split("/")[0],
-    })),
-    "relativePath"
+    data.allMdx.nodes.map(story => {
+      const parentFolder = story.slug.split("/")
+      return {
+        ...story,
+        parentFolder: parentFolder.length > 1 ? parentFolder[0] : "/",
+      }
+    }),
+    "parentFolder"
   )
 
-  Object.keys(stories).map(story => {
+  Object.keys(stories).map(story =>
     createPage({
       path: story,
       component: require.resolve("./src/templates/story.js"),
       context: stories[story].map(chapter => removeEmpty(chapter)),
     })
-  })
+  )
 }
